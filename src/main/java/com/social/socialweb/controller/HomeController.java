5777a70854f0b501b2ec4ab78efdc74174950dac
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-29 21:53:04 +0530

    Test getmapping

---
+++ src/main/java/com/social/socialweb/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.social.socialweb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String homeControllerHandler(){
+        return "This is a Home controller";
+    }
+}
