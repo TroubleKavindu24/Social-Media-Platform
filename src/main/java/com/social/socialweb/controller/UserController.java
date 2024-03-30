d27c56a951356e253942c2e405ae33b196e1bc15
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-30 12:20:07 +0530

    delete method (demo)

--- src/main/java/com/social/socialweb/controller/UserController.java
+++ src/main/java/com/social/socialweb/controller/UserController.java
@@ -3,6 +3,7 @@ package com.social.socialweb.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,12 @@ public class UserController {
         }
 
         return user1;
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable("userId") Integer userId){
+        
+
+        return "User deleted ! "+userId;
     }
 }
