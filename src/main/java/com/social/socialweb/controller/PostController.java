05f64dfe58ca9f00886e586b183628f490aee9c1
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-04-20 19:35:44 +0530

    change method to create post by token

--- src/main/java/com/social/socialweb/controller/PostController.java
+++ src/main/java/com/social/socialweb/controller/PostController.java
@@ -11,21 +11,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.social.socialweb.models.Post;
+import com.social.socialweb.models.User;
 import com.social.socialweb.response.ApiResponse;
 import com.social.socialweb.service.PostService;
+import com.social.socialweb.service.UserService;
 
 @RestController
 public class PostController {
     @Autowired
     PostService postService;
 
-    @PostMapping("/posts/user/{userId}")
-    public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable Integer userId)throws Exception{
+    @Autowired
+    UserService userService;
 
-        Post createdPost = postService.createNewPost(post, userId);
+    @PostMapping("/api/posts")
+    public ResponseEntity<Post> createPost(@RequestHeader("Authorization") String jwt, @RequestBody Post post)throws Exception{
+
+        User reqUser = userService.findUserByJwt(jwt);
+        Post createdPost = postService.createNewPost(post, reqUser.getId());
 
         return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
     }
