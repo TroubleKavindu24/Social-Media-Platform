3dc04fd169cb188ea8ac9ec65ed4d54c04fed277
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-04-20 13:47:17 +0530

    Implement logging token

--- src/main/java/com/social/socialweb/config/jwtValidator.java
+++ src/main/java/com/social/socialweb/config/jwtValidator.java
@@ -4,20 +4,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.social.socialweb.models.User;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
--- src/main/java/com/social/socialweb/controller/AuthController.java
+++ src/main/java/com/social/socialweb/controller/AuthController.java
@@ -2,37 +2,41 @@ package com.social.socialweb.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.social.socialweb.config.JwtConstant;
 import com.social.socialweb.config.JwtProvider;
 import com.social.socialweb.models.User;
 import com.social.socialweb.repository.UserRepository;
 import com.social.socialweb.request.LoginRequest;
 import com.social.socialweb.response.AuthResponse;
-import com.social.socialweb.service.UserService;
+//import com.social.socialweb.service.UserService;
+import com.social.socialweb.service.CustomerUserDetailsService;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
+    // @Autowired
+    // private UserService userService;
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetails;
 
     @PostMapping("/signup")
     public AuthResponse createUser(@RequestBody User user) throws Exception{
@@ -58,8 +62,29 @@ public class AuthController {
 
         return res;
     }
+
+    //auth/signin
+    @PostMapping("/signin")
     public AuthResponse signin(@RequestBody LoginRequest loginRequest){
-        return null;
+        Authentication authentication = authentication(loginRequest.getEmail(), loginRequest.getPassword());
+        
+        String token = JwtProvider.generateToken(authentication);
+    
+        AuthResponse res = new AuthResponse(token, "Login Success");
+
+        return res;
+    }
+    private Authentication authentication(String email, String password) {
+        UserDetails userDetails = customerUserDetails.loadUserByUsername(email);
+
+        if (userDetails==null) {
+            throw new BadCredentialsException("Invalid Username");
+        }
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Password not matched");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
     
 }
