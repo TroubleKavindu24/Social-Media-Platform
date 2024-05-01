a63f329afe9eed6633ba30a8b08a38fb93694d38
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-05-01 10:50:10 +0530

    implement global exception body

--- src/main/java/com/social/socialweb/exceptions/GlobalExceptions.java
+++ src/main/java/com/social/socialweb/exceptions/GlobalExceptions.java
@@ -1,5 +1,8 @@
 package com.social.socialweb.exceptions;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -10,6 +13,7 @@ public class GlobalExceptions {
     
     public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ue, WebRequest req){
 
-        return null;
+        ErrorDetails error = new ErrorDetails(ue.getMessage(),req.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<ErrorDetails>(error,HttpStatus.BAD_REQUEST);
     }
 }
